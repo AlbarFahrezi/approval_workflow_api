c6222d260cb0e4b25d152471a2289cd73f0d2de8
AlbarFahrezi <albarfahrezi7@gmail.com>
2026-08-21 15:49:57 +0700

    fix: add cors configuration manually for vercel frontend

---
+++ config/cors.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://approval-workflow-frontend.vercel.app',
+        'https://approval-workflow-frontend-q9qf.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
